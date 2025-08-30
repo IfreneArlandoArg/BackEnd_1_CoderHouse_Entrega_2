ac4f883dbb3d64c20e14330b683cd91c823b312c
Arlando A. Ifrene <arlandoifrene@gmail.com>
2025-08-30 00:59:30 -0300

    - Updated package.json to include express-handlebars and socket.io. - Configured express to use Handlebars as the view engine. - Created a new views router to handle rendering of views. - Added home.handlebars and realTimeProducts.handlebars templates for the views.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 import express from 'express';
+import { engine } from 'express-handlebars';
 import productsRouter from './routes/products.router.js';
 import cartsRouter from './routes/carts.router.js';
+import viewsRouter from './routes/views.router.js';
 
 const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './src/views');
 
 app.use(express.json());
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
+app.use('/', viewsRouter);
 
 app.listen(8080, () => {
   console.log('Servidor escuchando en el puerto 8080');
 });
+
